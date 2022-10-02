2e7465da7e3152faa2827b6601cca1721aaa232d
Ravis96 <ravis96@spoko.pl>
2022-10-02 13:01:51 +0200

    Fixed comment in MenuPlayerSetup, release 0.3.3

--- serdes-bukkit/src/main/java/cc.dreamcode.menu.serdes.bukkit/setup/MenuPlayerSetup.java
+++ serdes-bukkit/src/main/java/cc.dreamcode.menu.serdes.bukkit/setup/MenuPlayerSetup.java
@@ -4,7 +4,7 @@ import lombok.NonNull;
 import org.bukkit.entity.HumanEntity;
 
 /**
- * Interface to set up custom paginated menu from config.
+ * Interface to set up custom player menu from config.
  * Here you can apply event on slots, add custom items in real-time and what you want.
  * This class works like consumer class.
  */
