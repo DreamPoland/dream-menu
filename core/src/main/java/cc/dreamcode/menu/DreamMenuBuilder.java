617c02b2d656f59beed742ccdccac53562eca9c9
Ravis96 <ravis96@spoko.pl>
2024-07-01 16:07:40 +0200

    Add fill-item builder methods

--- core/src/main/java/cc/dreamcode/menu/DreamMenuBuilder.java
+++ core/src/main/java/cc/dreamcode/menu/DreamMenuBuilder.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 @Getter
 @RequiredArgsConstructor
@@ -34,6 +35,71 @@ public abstract class DreamMenuBuilder<B, T, I> {
             if (!this.items.containsKey(slot)) {
                 this.items.put(slot, i);
             }
+        }
+
+        return this;
+    }
+
+    public DreamMenuBuilder<B, T, I> fillTop(@NonNull I i) {
+        Stream.of(
+                MenuUtil.countSlot(1, 1),
+                MenuUtil.countSlot(1, 2),
+                MenuUtil.countSlot(1, 3),
+                MenuUtil.countSlot(1, 4),
+                MenuUtil.countSlot(1, 5),
+                MenuUtil.countSlot(1, 6),
+                MenuUtil.countSlot(1, 7),
+                MenuUtil.countSlot(1, 8),
+                MenuUtil.countSlot(1, 9)
+        ).forEach(slot -> {
+            if (!this.items.containsKey(slot)) {
+                this.items.put(slot, i);
+            }
+        });
+
+        return this;
+    }
+
+    public DreamMenuBuilder<B, T, I> fillBottom(@NonNull I i) {
+        Stream.of(
+                MenuUtil.countSlot(this.rows, 1),
+                MenuUtil.countSlot(this.rows, 2),
+                MenuUtil.countSlot(this.rows, 3),
+                MenuUtil.countSlot(this.rows, 4),
+                MenuUtil.countSlot(this.rows, 5),
+                MenuUtil.countSlot(this.rows, 6),
+                MenuUtil.countSlot(this.rows, 7),
+                MenuUtil.countSlot(this.rows, 8),
+                MenuUtil.countSlot(this.rows, 9)
+        ).forEach(slot -> {
+            if (!this.items.containsKey(slot)) {
+                this.items.put(slot, i);
+            }
+        });
+
+        return this;
+    }
+
+    public DreamMenuBuilder<B, T, I> fillTopAndBottom(@NonNull I i) {
+        this.fillTop(i);
+        this.fillBottom(i);
+
+        return this;
+    }
+
+    public DreamMenuBuilder<B, T, I> fillMargin(@NonNull I i) {
+        this.fillTop(i);
+        this.fillBottom(i);
+
+        for (int row = 2; row < this.rows; row++) {
+            Stream.of(
+                    MenuUtil.countSlot(row, 1),
+                    MenuUtil.countSlot(row, 9)
+            ).forEach(slot -> {
+                if (!this.items.containsKey(slot)) {
+                    this.items.put(slot, i);
+                }
+            });
         }
 
         return this;
